28fd8f2abf463da1854852e4a76fd47e7d062c87
Isaji-withchrist <okarekpefavour@gmail.com>
2025-07-11 11:30:33 +0100

    initial commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -14,9 +15,15 @@ function App() {
   return (
     
       <div> 
-      <Header/>
-      <MainContent/>
-      <Footer/>
+        <div>
+          <h1>User Profile Example</h1>
+          <UserProfile
+          name= "Alice"
+          age= "25"
+          bio= "Loves hiking and photography"
+          />
+        </div>
+      
         </div>
     
   )
