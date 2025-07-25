a9ff79cff8894049dcfacfb9b2647b381ec21496
Isaji-withchrist <okarekpefavour@gmail.com>
2025-07-25 17:23:10 +0100

    initial commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
@@ -8,6 +8,7 @@ import EditRecipeForm from './components/EditRecipeForm';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="p-4">
       <Routes>
         <Route path="/" element={<RecipeList />} />
@@ -16,6 +17,7 @@ function App() {
         <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
       </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
