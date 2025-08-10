eb78ab652c9fa68f1232510e2dbbaede16abf804
Isaji-withchrist <okarekpefavour@gmail.com>
2025-08-10 23:54:53 +0100

    add githubServices.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchUserData = async (username) => {
     try{
         const response = await
-        axios.get ('https:/apigithub.com/users/${username}');
+        axios.get ('https:/api.github.com/users/${username}');
         return response.data;
     } catch (error){
         throw new Error('User not found');
