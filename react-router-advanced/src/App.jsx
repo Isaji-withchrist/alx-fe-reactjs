6c416ad021c1f3b55d54b62759aeef8e7f0e17ea
Isaji-withchrist <okarekpefavour@gmail.com>
2025-08-19 18:36:31 +0100

    router advanced

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,27 +1,23 @@
 // src/App.jsx
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import ProfileDetails from "./pages/ProfileDetails";
+import ProfileSettings from "./pages/ProfileSettings";
 import BlogPost from "./pages/BlogPost";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <nav className="p-4 bg-gray-200 flex gap-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/profile">Profile</Link>
-        <Link to="/blog/1">Blog Post #1</Link>
-      </nav>
-
+    <BrowserRouter>
       <Routes>
+        {/* Basic routes */}
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
 
-        {/* Protected Profile route */}
+        {/* Protected route */}
         <Route
           path="/profile/*"
           element={
@@ -29,14 +25,23 @@ export default function App() {
               <Profile />
             </ProtectedRoute>
           }
-        />
+        >
+          {/* Nested routes under Profile */}
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
 
-        {/* Dynamic blog post route */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        {/* Dynamic route for blog posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
 
-        {/* Login */}
+        {/* Auth route */}
         <Route path="/login" element={<Login />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
+
+export default App;
